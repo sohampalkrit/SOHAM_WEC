31ba12b945905fcd589d39eb12745503aa6f0dc5
sohampalkrit <sohampalkrit05@gmail.com>
2024-10-11 06:05:03 +0530

    initial commit

--- Docker-entrypoint.sh
+++ Docker-entrypoint.sh
@@ -1,10 +1,16 @@
+#for specifying to os that the file is to executed with sh 
+#!/bin/sh
+
+#for error dectection with exited with non zero then return 
 set -e
 
+#checks if the bundle in updated all the dependencies are installed with the gemfile 
 if ! [bundle check ] ; then 
     bundle install
 fi
 
+#forcefully removes a potentially stale server.pid file
 rm -f /app/tmp/pids/server.pid
 
-
+#tells the shell to replace itself with whatever command is passed as an argument to the script.
 exec "$@"
